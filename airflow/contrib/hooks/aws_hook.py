66bebd7b864646533127f4114e922c7ac4175e36
sid.gupta <sid.gupta@glassdoor.com>
2017-09-30 08:44:33 +0200

    [AIRFLOW-1560] Add AWS DynamoDB hook and operator for inserting batch items
    Closes #2587 from
    sid88in/feature/dynamodb_hook_and_operator

--- airflow/contrib/hooks/aws_hook.py
+++ airflow/contrib/hooks/aws_hook.py
@@ -137,3 +137,25 @@ class AwsHook(BaseHook):
             aws_secret_access_key=aws_secret_access_key,
             endpoint_url=endpoint_url
         )
+
+    def get_resource_type(self, resource_type, region_name=None):
+        try:
+            connection_object = self.get_connection(self.aws_conn_id)
+            aws_access_key_id = connection_object.login
+            aws_secret_access_key = connection_object.password
+
+            if region_name is None:
+                region_name = connection_object.extra_dejson.get('region_name')
+
+        except AirflowException:
+            # No connection found: fallback on boto3 credential strategy
+            # http://boto3.readthedocs.io/en/latest/guide/configuration.html
+            aws_access_key_id = None
+            aws_secret_access_key = None
+
+        return boto3.resource(
+            resource_type,
+            region_name=region_name,
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
